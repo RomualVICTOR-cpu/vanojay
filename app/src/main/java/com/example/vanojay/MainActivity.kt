ef209321445ba3343406619e9be92ab50c4c10c1
Victor Romual <vanojayleguitariste@icloud.com>
2025-11-20 22:01:05 +0100

    lab1

---
+++ app/src/main/java/com/example/vanojay/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.vanojay
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.question).setOnClickListener {
+            findViewById<View>( R.id.question).visibility = View.VISIBLE
+            findViewById<View>(R.id.reponse).visibility = View.INVISIBLE
+        }
+
+        findViewById<View>(R.id.reponse).setOnClickListener {
+            findViewById<View>(R.id.question).visibility = View.INVISIBLE
+            findViewById<View>( R.id.reponse).visibility = View.VISIBLE
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
